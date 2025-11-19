ca867ebcf5e00f895ac8c21d6bf8519a0444e8c7
kareem-dev-del <krym33632@gmail.com>
2025-11-19 13:05:57 +0200

    Update

--- code.c
+++ code.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include <math.h>
+
 int main() {
- double number, power, result;
- printf("Enter a number: ");
- scanf("%f", &number);
- printf("Enter the power: ");
- scanf("%lf", &power);
- result = pow(number, power);
- printf("%.2lf raised to %.2lf = %.2lf\n", number, power, result);
- printf("Square root of %.2lf = %.2lf\n", number, sqrt(number));
+   char x[50] = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNO123456789";
+   printf("%lu",sizeof(x));
+
  return 0; }
+
+ 
