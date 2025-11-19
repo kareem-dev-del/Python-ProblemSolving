f4a19c511adc199873d81ebc5bf0ae376ccd4400
kareem-dev-del <krym33632@gmail.com>
2025-11-19 16:44:49 +0200

    Update

--- code.c
+++ code.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
+int maxArray(int arr[],int size){
+  int max=arr[0];
+  for(int i=0;i<size;i++){
+    if (arr[i] > max){
+      max=arr[i];
+    }
+  }
+  return max;
+}
 int main() {
-  int arr[] = {1, 2, 3}; 
-  int p=*arr;
-  printf("%d", p + 5);
+ 
+  int array[]={1,2,3,4,5};
+  int sizee= sizeof(array) / sizeof(array[0]);
+  printf("tne max:%d",maxArray(array,sizee));
 
- return 0; }
+  
+return 0; }
 
  
