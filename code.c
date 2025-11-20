03ee45d05b13c3a743c41a3deed7e8976f0376da
kareem-dev-del <krym33632@gmail.com>
2025-11-20 18:04:22 +0200

    Update

--- code.c
+++ code.c
@@ -1,54 +1,15 @@
-#include <stdio.h>
-
-// int maxArray(int arr[],int size){
-//   int max=arr[0];
-//   for(int i=0;i<size;i++){
-//     if (arr[i] > max){
-//       max=arr[i];
-//     }
-//   }
-//   return max;
-// }
-
-
-// float avr(int x, int y , int z){
-//   return (x+ y +z )/ 3.0;
-// }
-
-// int Fib (int n){
-//   if (n == 0 || n == 1) return n;
-//    return  Fib(n-1) + Fib(n-2) ;
-
-// }
-
-
-
-float L(int km){
-  return km * 0.621371;
-}
-
-
-
-int main() {
- 
-  // int array[]={1,2,3,4,5};
-  // int sizee= sizeof(array) / sizeof(array[0]);
-  // printf("tne max:%d",maxArray(array,sizee));
-
-  // float x=5,y=4,z=1;
-  // printf("The avrege:%.2f",avr(x,y,z));
-// int x=5;
-// int *p=&x;
-// printf("value:%d\n",x);
-// printf("adress of integers:%p\n",&x);
-// printf("valve store poin: %d\n",*p);
-// printf("adress of poin:%p",&p);
-  // int num=5;
-  // printf("the Fibonacci:%d",Fib(num));
- int arr[]={1,5,10};
- for(int i=0;i<3;i++){
-  printf("%d:km converts %.3f miles: \n",arr[i],L(arr[i]));
+ #include <stdio.h>
+ int main() {
+//  int x = 10;
+//  int *p = &x;
+//  printf("&pValue of x: %d\n", x);
+//  printf("*pAddress of x: %p\n", &x);
+//  printf("Value stored at pointer p: %p\n", p);
+//  printf("the Value that pointer p points to is: %d\n", *p);
+//  printf("Address of pointer: %p\n", &p);
+int x[5]={10,20,30,40,50};
+int *p=x;
+printf("%d\n",*(p + 2));
+printf("%d",strlen(x));
+ return 0;
  }
-return 0; }
-
- 
