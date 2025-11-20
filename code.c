614fd9d95f124cfe7fb84cd740826381b0547b7d
kareem-dev-del <krym33632@gmail.com>
2025-11-20 18:06:12 +0200

    Update

--- code.c
+++ code.c
@@ -7,9 +7,9 @@
 //  printf("Value stored at pointer p: %p\n", p);
 //  printf("the Value that pointer p points to is: %d\n", *p);
 //  printf("Address of pointer: %p\n", &p);
-int x[5]={10,20,30,40,50};
-int *p=x;
-printf("%d\n",*(p + 2));
-printf("%d",strlen(x));
+// int x[5]={10,20,30,40,50};
+// int *p=x;
+// printf("%d\n",*(p + 2));
+// printf("%d",strlen(x));
  return 0;
  }
