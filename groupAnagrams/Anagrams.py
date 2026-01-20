f5d3e1dc9401de8f4ef214fe39fe990664b56a2d
kareem-dev-del <krym33632@gmail.com>
2026-01-20 14:24:09 +0200

    Update

--- groupAnagrams/Anagrams.py
+++ groupAnagrams/Anagrams.py
@@ -1,17 +1,32 @@
 # البرنامج يجمع كلمات Anagrams مع بعضها في مجموعات حسب ترتيب الحروف
 # الدالة groupAnagrams تستقبل أي عدد من الكلمات كـ arguments باستخدام *args
 
-def groupAnagrams(*words):
-    print("all words", words)
-    if len(words) < 2:
-        return [[]]  
 
-    anagrams = {}
-    
-    for word in words:
-        key = ''.join(sorted(word))      
-        if key not in anagrams:          
-            anagrams[key] = []
-        anagrams[key].append(word)       
 
-    return list(anagrams.values())       
+# dict: لازم تتحقق من المفتاح قبل الإضافة
+
+# defaultdict: المفتاح يتخلق تلقائيًا مع القيمة الافتراضية
+
+
+
+from collections import defaultdict
+
+words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+anagrams = defaultdict(list)
+
+for word in words:
+    key = ''.join(sorted(word))  # ترتيب الحروف
+    anagrams[key].append(word)
+
+print(anagrams)
+
+#other solve==================================
+# anagrams={}
+# for word in words:
+#     key = ''.join(sorted(word))  # ترتيب الحروف
+#     if  key  not in anagrams:
+#       anagrams[key]= []
+#     anagrams[key].append(word)
+
+# print(anagrams)
