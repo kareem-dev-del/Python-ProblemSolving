f11e8204841700b8b18b0dace99899370ac48926
kareem-dev-del <krym33632@gmail.com>
2026-01-21 15:53:26 +0200

    pdate

---
+++ solve.py
@@ -0,0 +1,34 @@
+
+
+
+def x(nums):
+   count= {}
+   items= []
+   result = []
+
+   
+   for num in nums:
+        if num in count:
+            count[num] += 1
+        else:
+           count[num] = 1  
+
+
+   for key in count:
+       items.append([key,count[key]])
+
+
+   for _ in range(1):
+       max_freq=-1
+       max_item=None
+       for item in items:
+           if item[1] > max_freq:
+               max_freq=item[1]
+               max_item=item
+          
+       result.append(max_item)
+       items.remove(max_item) 
+   return result  
+
+    
+print(x([1,2,3,2,4,5,1]) ) 
