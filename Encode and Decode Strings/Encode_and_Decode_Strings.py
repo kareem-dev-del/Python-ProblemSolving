215e50582638d258aa52744dcffcb2d1a3946301
kareem-dev-del <krym33632@gmail.com>
2026-01-22 20:59:50 +0200

    Update

---
+++ Encode and Decode Strings/Encode_and_Decode_Strings.py
@@ -0,0 +1,42 @@
+# encode: بنحوّل كل كلمة لشكل طولها#الكلمة ونلزقهم كلهم في String واحد عشان نبعته بأمان.
+
+# decode: بنقرأ الرقم قبل # عشان نعرف طول الكلمة، وبناخد العدد ده من الحروف بعده ونكرر لحد ما النص يخلص.
+
+#Day=>Three
+
+
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for s in strs:
+            encoded += str(len(s)) + "#" + s
+        return encoded    
+
+    def decode(self, s: str) -> List[str]:
+        result = []
+        i = 0
+
+        while i < len(s):
+            hash_index = s.find('#', i)
+            if hash_index == -1:
+                break
+            length = int(s[i:hash_index])
+            word = s[hash_index + 1: hash_index + 1 + length]
+            result.append(word)
+            i = hash_index + 1 + length
+        return result
+
+# اختبار الكود
+if __name__ == "__main__":
+    sol = Solution()  # إنشاء كائن من الكلاس
+    
+    # اختبار التشفير
+    encoded_str = sol.encode(["kareem", "Ali"])
+    print("Encoded:", encoded_str)  # Output: 7#kareem3#Ali
+    
+    # اختبار فك التشفير
+    decoded_list = sol.decode(encoded_str)
+    print("Decoded:", decoded_list)  # Output: ['kareem', 'Ali']
