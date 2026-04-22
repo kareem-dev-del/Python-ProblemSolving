7717520662ca713331ac8d5e627457288e330238
kareem-dev-del <krym33632@gmail.com>
2026-04-22 22:41:53 +0200

    Add gitignore

---
+++ part1.py
@@ -0,0 +1,20 @@
+def Palindrome(word):
+   
+    clean_input = ""
+
+    for c in word:
+        if c.isalnum():
+            clean_input += c.lower()
+    print(clean_input) 
+    start = 0
+    end = len(clean_input) - 1 
+    while( start < end ):
+        if clean_input[start] != clean_input[end]: 
+            return False
+        start+=1
+        end -=1
+    return True 
+
+#x="Was it a car or a cat I saw?"
+x="L?evel  .  kareem@"
+print(Palindrome(x))    
